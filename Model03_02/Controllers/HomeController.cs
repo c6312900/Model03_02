1b4336da500c95cde60764de18e85c0287087863
c6312900 <c6312900@gmail.com>
2017-05-11 18:36:41 +0800

    Model03_02

---
+++ Model03_02/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Model03_02.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            ViewData["data1"] = DateTime.Now;
+            ViewBag.data2 = DateTime.Now;
+            return View();
+        }
+
+        public ActionResult Index1()
+        {
+
+            ViewData["data1"] = DateTime.Now;
+            ViewBag.data2 = DateTime.Now;
+            return View("Index");
+        }
+    }
+}
---
+++ Model03_02/Controllers/OperaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Model03_02.Controllers
+{
+    public class OperaController : Controller
+    {
+        // GET: Opera
+        public ActionResult Index()
+        {
+
+            ViewBag.OperaID = 1;
+            ViewBag.Title = "XXXX";
+            return View();
+        }
+    }
+}
